860152a5677a90b43e9041dc32f93670784b32af
Mr-Lobaloba <maharajaah@gmail.com>
2023-11-24 20:18:17 +0200

    Added 3 maps

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
+    WA.controls.disablePlayerProximityMeeting();
+
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
         const time = today.getHours() + ":" + today.getMinutes();
@@ -18,6 +20,80 @@ WA.onInit().then(() => {
     })
 
     WA.room.area.onLeave('clock').subscribe(closePopup)
+
+    WA.room.area.onEnter('phrase01').subscribe(() => {
+        currentPopup = WA.ui.openPopup("phrase01Popup", "Welcome to Jalan Journey!", []);
+    })
+
+    WA.room.area.onLeave('phrase01').subscribe(closePopup)
+
+
+    WA.room.area.onEnter('phrase02').subscribe(() => {
+        currentPopup = WA.ui.openPopup("phrase02Popup", "These are woka - characters in the game as well! Remember to interact with them and you might find something interesting!", []);
+    })
+
+    WA.room.area.onLeave('phrase02').subscribe(closePopup)
+
+
+    WA.room.area.onEnter('phrase03').subscribe(() => {
+        currentPopup = WA.ui.openPopup("phrase03Popup", "Look out for these green arrows! They are portals into the next rooms. Try to enter", []);
+    })
+
+    WA.room.area.onLeave('phrase03').subscribe(closePopup)
+
+    WA.room.area.onEnter('phrase04').subscribe(() => {
+        currentPopup = WA.ui.openPopup("phrase04Popup", "What to do when you are lost?", []);
+    })
+
+    WA.room.area.onLeave('phrase04').subscribe(closePopup)
+
+    WA.room.area.onEnter('phrase05').subscribe(() => {
+        currentPopup = WA.ui.openPopup("phrase05Popup", "If you ever find yourself experiencing some sort of glitch, just refresh your browser page and you'll find yourself at where you last left off!", []);
+    })
+
+    WA.room.area.onLeave('phrase05').subscribe(closePopup)
+
+    WA.room.area.onEnter('phrase06').subscribe(() => {
+        currentPopup = WA.ui.openPopup("phrase06Popup", "How are you? Auntie is happy!", []);
+    })
+
+    WA.room.area.onLeave('phrase06').subscribe(closePopup)
+
+    WA.room.area.onEnter('phrase07').subscribe(() => {
+        currentPopup = WA.ui.openPopup("phrase07Popup", "Please have a seat and start video meeting", []);
+    })
+
+    WA.room.area.onLeave('phrase07').subscribe(closePopup)
+
+    WA.room.area.onEnter('phrase08').subscribe(() => {
+        currentPopup = WA.ui.openPopup("phrase08Popup", "You can change the aspect ratio by zooming in and out using your mouse or trackpad!", []);
+    })
+
+    WA.room.area.onLeave('phrase08').subscribe(closePopup)
+
+    WA.room.area.onEnter('phrase09').subscribe(() => {
+        currentPopup = WA.ui.openPopup("phrase09Popup", "These blue arrows will guide you from one location to another!", []);
+    })
+
+    WA.room.area.onLeave('phrase09').subscribe(closePopup)
+
+    WA.room.area.onEnter('phrase10').subscribe(() => {
+        currentPopup = WA.ui.openPopup("phrase10Popup", "Lastly, do ask your facilitator for help if you require any assistance!", []);
+    })
+
+    WA.room.area.onLeave('phrase10').subscribe(closePopup)
+
+
+
+    WA.room.area.onEnter('phrase11').subscribe(() => {
+        currentPopup = WA.ui.openPopup("phrase11Popup", "Hi i'm Jason. Want to have a conversation?", []);
+    })
+
+    WA.room.area.onLeave('phrase11').subscribe(closePopup)
+
+
+
+
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
@@ -33,4 +109,4 @@ function closePopup(){
     }
 }
 
-export {};
+
