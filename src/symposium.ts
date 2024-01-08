e28382d12072546d3d13301fa45ec78a3a206835
IcedKlementi <143535114+JJProduct@users.noreply.github.com>
2024-01-08 22:09:16 +0800

    Fixed import statements

--- src/symposium.ts
+++ src/symposium.ts
@@ -1,1 +1,30 @@
+/// <reference types="@workadventure/iframe-api-typings" />
+
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+
+console.log('Script started successfully');
+
+let currentPopup: any = undefined;
+
+// Waiting for the API to be ready
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags)
+
+    WA.controls.disablePlayerProximityMeeting();
+
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+
+}).catch(e => console.error(e));
+
+function closePopup(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
+
+
