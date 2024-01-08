54a6fd7f92aa71ba8ac9a5f44611a0103449bfd6
IcedKlementi <143535114+JJProduct@users.noreply.github.com>
2024-01-08 22:12:04 +0800

    Removed current popup def

--- src/street.ts
+++ src/street.ts
@@ -4,8 +4,6 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
-
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
